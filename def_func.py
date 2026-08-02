a2a96dc504fc08f887552b39fcf33e63ade1ca40
apran <apranitchii@gmail.com>
2026-08-02 10:09:00 -0400

    add new file and code

---
+++ def_func.py
@@ -0,0 +1,41 @@
+
+from fractions import Fraction
+from math import remainder
+from unittest import result
+
+first_input = input("Enter first fraction: ")
+operation = input("Enter operation (+, -, *, /): ")
+second_input = input("Enter second fraction: ")
+
+first_fraction = Fraction(first_input)
+second_fraction = Fraction(second_input)
+
+if operation == "+":
+    result = first_fraction + second_fraction
+
+elif operation == "-":
+    result = first_fraction - second_fraction
+
+elif operation == "*":
+    result = first_fraction * second_fraction
+
+elif operation == "/":
+    result = first_fraction / second_fraction
+
+else:
+    print("Unknown operation")
+    exit()
+
+whole = result.numerator // result.denominator
+remainder = result.numerator % result.denominator
+
+if remainder == 0:
+    print(f"Result: {whole}")
+
+else:
+    print(f"Result: {whole} / {remainder}/{result.denominator}")
+
+
+
+
+
